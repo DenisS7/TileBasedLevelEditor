5bcbdf1e5b2d36c3f49a642b64282af670e32c95
DenisS7 <220326@buas.nl>
2025-06-04 22:47:09 +0200

    Base classes for (relay) command and view model
    Added base classes for command, relay command and the view model

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Commands
+{
+    public class RelayCommand : CommandBase
+    {
+        private readonly Action<object?> execute;
+        private readonly Func<object?, bool>? canExecute;
+
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            if (canExecute is not null)
+                return canExecute(parameter);
+
+            return base.CanExecute(parameter);
+        }
+
+        public override void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
