470faf530d612c41ce76bf7ab7461aa6b211b757
DenisS7 <220326@buas.nl>
2025-06-08 00:34:10 +0200

    Tilemap and tile data model
    Added model for tilemap and tile data

---
+++ Models/TileData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TileBasedLevelEditor.Models
+{
+    class TileData
+    {
+        public Vec2<int> TilesetIndex { get; set; }
+        public CroppedBitmap TileImage { get; set; }
+
+        public TileData(Vec2<int> tilesetIndex, CroppedBitmap tileImage)
+        {
+            TilesetIndex = tilesetIndex;
+            TileImage = tileImage;
+        }
+    }
+
+}
---
+++ Models/Tilemap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TileBasedLevelEditor.Models
+{
+    class Tilemap
+    {
+        public string Name { get; }
+
+        public Vec2<int> TileSize { get; }
+
+        public Vec2<int> TilemapSize { get; }
+
+        public Dictionary<Vec2<int>, TileData> Tiles { get; }
+
+        public Tilemap(string name, Vec2<int> tileSize, Vec2<int> tilemapSize)
+        {
+            Name = name;
+            TileSize = tileSize;
+            TilemapSize = tilemapSize;
+            Tiles = new Dictionary<Vec2<int>, TileData>(tilemapSize.X * tilemapSize.Y);
+        }
+
+        public void SetTile(Vec2<int> tilesetIndex, Vec2<int> tilemapIndex, CroppedBitmap tileImage)
+        {
+            int tileIndex = tilemapIndex.Y * TilemapSize.X + tilemapIndex.X;
+            if (0 > tileIndex || tileIndex >= Tiles.Count)
+                return;
+
+            Tiles[tilemapIndex].TilesetIndex = tilesetIndex;
+            Tiles[tilemapIndex].TileImage = tileImage;
+        }
+    }
+}
