90f31ac046d404ba7cf740db0c0c6602bd582dc9
DenisS7 <220326@buas.nl>
2025-06-22 22:59:14 +0200

    Tile grid dragging (with unwanted behavior)
    Added dragging across the tile grid
    Unwanted behavior of dragging the tile grid when it is fully visible or dragging it outside of view

---
+++ Misc/DragBehavior.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TileBasedLevelEditor.Misc
+{
+    //from https://stackoverflow.com/questions/27865441/move-items-in-a-canvas-using-mvvm
+    public class DragBehavior
+    {
+        public readonly TranslateTransform Transform = new TranslateTransform();
+        private Point _elementStartPosition2;
+        private Point _mouseStartPosition2;
+        private bool _isDragging = false;
+        private static DragBehavior _instance = new DragBehavior();
+        public static DragBehavior Instance
+        {
+            get { return _instance; }
+            set { _instance = value; }
+        }
+
+        public static bool GetDrag(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsDragProperty);
+        }
+
+        public static void SetDrag(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsDragProperty, value);
+        }
+
+        public static readonly DependencyProperty IsDragProperty =
+          DependencyProperty.RegisterAttached("Drag",
+          typeof(bool), typeof(DragBehavior),
+          new PropertyMetadata(false, OnDragChanged));
+
+        private static void OnDragChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var element = (UIElement)sender;
+            var isDrag = (bool)(e.NewValue);
+
+
+            Instance = new DragBehavior();
+            element.RenderTransform = Instance.Transform;
+
+            if (isDrag)
+            {
+                element.MouseRightButtonDown += Instance.ElementOnMouseRightButtonDown;
+                element.MouseRightButtonUp += Instance.ElementOnMouseRightButtonUp;
+                element.MouseMove += Instance.ElementOnMouseMove;
+            }
+            else
+            {
+                element.MouseRightButtonDown -= Instance.ElementOnMouseRightButtonDown;
+                element.MouseRightButtonUp -= Instance.ElementOnMouseRightButtonUp;
+                element.MouseMove -= Instance.ElementOnMouseMove;
+            }
+        }
+
+        private void ElementOnMouseRightButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            var parent = Application.Current.MainWindow;
+            _mouseStartPosition2 = mouseButtonEventArgs.GetPosition(parent);
+            ((UIElement)sender).CaptureMouse();
+            _isDragging = true;
+        }
+
+        private void ElementOnMouseRightButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            ((UIElement)sender).ReleaseMouseCapture();
+            _elementStartPosition2.X = Transform.X;
+            _elementStartPosition2.Y = Transform.Y;
+            _isDragging = false;
+        }
+
+        private void ElementOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (!((UIElement)sender).IsMouseCaptured || !_isDragging) return;
+
+            var parent = Application.Current.MainWindow;
+            var mousePos = mouseEventArgs.GetPosition(parent);
+            var diff = (mousePos - _mouseStartPosition2);
+            Transform.X = _elementStartPosition2.X + diff.X;
+            Transform.Y = _elementStartPosition2.Y + diff.Y;
+            Debug.WriteLine("Transform: {0}, {1}", Transform.X, Transform.Y);
+        }
+    }
+}
